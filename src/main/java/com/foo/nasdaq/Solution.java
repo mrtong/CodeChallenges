b987360376592859814c52280fd4d1b56bb2e3c9
lt093683 <luke.tong@vodafone.com.au>
2019-01-26 14:32:11 +1100

    change method names to make it more readable

--- src/main/java/com/foo/nasdaq/Solution.java
+++ src/main/java/com/foo/nasdaq/Solution.java
@@ -5,21 +5,21 @@ import java.math.BigInteger;
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.printOutResult(5, 2);
-        s.printOutResult(100, 10);
-        s.printOutResult(1000000, 200);
-        s.printOutResult(10000000, 200);
+        s.calConsecutiveNumbers(5, 2);
+        s.calConsecutiveNumbers(100, 10);
+        s.calConsecutiveNumbers(1000000, 200);
+        s.calConsecutiveNumbers(10000000, 200);
     }
 
 
-    public String printOutResult(int n, int C) {
+    public String calConsecutiveNumbers(int n, int C) {
         String output = doJob(n, C).toString();
         System.out.printf("for n=%d & C=%d\nThe result is =%s\n\n", n, C,
                 output.substring(0, Math.min(10, output.length())));
         return output;
     }
 
-    public BigInteger doJob(int n, int C) {
+    private BigInteger doJob(int n, int C) {
         BigInteger product = new BigInteger("1");
         BigInteger sum = new BigInteger("0");
         for (int i = 1; i < n; i++) {
